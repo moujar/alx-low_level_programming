7c818eaea32f1d0c9da6f10f3be9c571cee3d430
Abderrahmane Moujar <abderrahmanemoujar@gmail.com>
2023-02-16 21:59:32 +0100

    first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+*	main - Print a string using puts
+*	Return: 0 is success
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
